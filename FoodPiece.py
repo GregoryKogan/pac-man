f12a6f8eb8182eaf8dc9d37cf4ebb8aa4b57d9b6
Gregory Kogan <60318411+GregoryKogan@users.noreply.github.com>
2020-07-21 03:17:50 +0300

    Delete FoodPiece.py

--- FoodPiece.py
+++
@@ -1,9 +0,0 @@
-class FoodPiece:
-    def __init__(self, i, j, type=None):
-        self.i = i
-        self.j = j
-        self.type = None
-        if type:
-            self.type = "Energizer"
-        else:
-            self.type = "Normal"
